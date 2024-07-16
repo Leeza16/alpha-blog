7a7e606b74e9b8cc6553c42ec605174989bc50ba
Leeza16 <leezarang@gmail.com>
2024-07-16 18:46:39 +0530

    Completed Article

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  # validates :title, presence: true, length: { minimum: 6, maximum: 100, message: 'Invalid Title'  }
+  # validates :description, presence: true, length: { minimum: 10 }
+end
