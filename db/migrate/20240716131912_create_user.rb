56d1ea76938d9259634baeab1678812da9f40d43
Leeza16 <leezarang@gmail.com>
2024-07-17 12:25:30 +0530

    Add one to many assoiations

--- db/migrate/20240716131912_create_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUser < ActiveRecord::Migration[7.1]
-  def change
-    create_table :users do |t|
-      t.string :title
-      t.string :description
-      t.timestamps
-    end
-  end
-end
